d0f1c55be79d65308b7852301623ee3314e4dd83
Lukas Plazovnik <lukas.plazovnik@gmail.com>
2018-10-27 16:20:45 +0200

    initial commit

---
+++ bot.js
@@ -0,0 +1,300 @@
+#!/usr/bin/env node
+////////////////////////////////////////////////////////////////////////////////////////
+//includes
+/////////////////////////////////////////////////////////////////////////////////////////
+var ArrayList = require('arraylist');
+var auth = require('./config/auth.json');
+var Discord = require('discord.js');
+var fs = require('fs');
+const { exec } = require('child_process');
+
+////////////////////////////////////////////////////////////////////////////////////////
+//variables
+/////////////////////////////////////////////////////////////////////////////////////////
+var subscribersBlacklist ;
+var dailyMessagedelivered;
+
+var newMemberfirstMasternodes=[];
+const DiscordBot = new Discord.Client();
+var messageTriggered = new Date();
+
+var helpers=require('./user_modules/Helpers');
+
+
+DiscordBot.on('ready', () => {
+ 	console.log(`Logged in as ${DiscordBot.user.tag}!`);
+
+});
+
+
+
+
+// setInterval(function() {
+// 	DiscordBot.channels.find("name", "general").send(JSON.parse(fs.readFileSync('reward.json', 'utf8')));
+// }, 36000000*3);
+
+
+//Monitor channel
+setInterval(function() {
+	exec(auth.cli+' getblockcount', function callback(error, stdout, stderr){
+										
+				    		var CB="";
+				    		var CE="";
+				    		var GX="";
+						process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+
+
+							helpers.get_json(auth.exchanges, function (resp) {
+
+					var varlist= [];
+	
+									CB=resp.exchangesBTC[0];								
+									CE=resp.exchangesBTC[1];								
+									GX=resp.exchangesBTC[2];								
+								
+								// console.log(CE.result);
+								// console.log(GX);
+								// console.log(cbvolume);
+
+
+							//toDo: this part is only supports 
+
+								 cbvolume=CB.volume.toPrecision(3);
+								 varlist.push({name: 'cbvolume',value: cbvolume});
+								cevolume=CE.volume.toPrecision(3);
+								 varlist.push({name: 'cevolume',value: cevolume});
+								gxvolume=GX.volume.toPrecision(3);
+								 varlist.push({name: 'gxvolume',value: gxvolume});
+
+
+								cbbuy=CB.buy;
+								cebuy=CE.buy;
+								gxbuy=GX.buy;
+
+								cbsell=CB.sell;
+								cesell=CE.sell;
+								gxsell=GX.sell;
+
+
+								 varlist.push({name: 'cesell',value: cesell});
+								 varlist.push({name: 'cbsell',value: cbsell});
+								 varlist.push({name: 'gxsell',value: gxsell});
+
+								 varlist.push({name: 'cbbuy',value: cbbuy});
+								 varlist.push({name: 'cebuy',value: cebuy});
+								 varlist.push({name: 'gxbuy',value: gxbuy});
+
+								volume=eval(cbvolume+"+"+cevolume+"+"+gxvolume).toPrecision(3);//
+								 varlist.push({name: 'volume',value: volume});
+								// volume=parseFloat(cbvolume)+parseFloat(cevolume)+parseFloat(gxvolume)
+							// 
+								monitormsg=fs.readFileSync('monitor.json', 'utf8');
+				    		// var temp_stak=fs.readFileSync('blockheight.json', 'utf8');
+								// console.log(monitormsg);
+
+
+
+
+								varlist.forEach(function(value){
+									var replace = "<"+value.name+">";
+									// console.log(replace);
+									regex=new RegExp(replace,"g");
+									// console.log(regex);
+									// test="<gxbuy>";
+									monitormsg=monitormsg.replace(regex, value.value);
+								});
+						exec(auth.exchanges+' getblockcount', function callback(error, stdout, stderr){
+										
+
+				    	setTimeout(function() {
+				    	
+								DiscordBot.channels.find("name", "monitor").send(JSON.parse(monitormsg.replace(/<block>/g, stdout.match(/\d+/))));
+						}, 1000 );
+
+						});
+
+   
+							});
+
+
+				    
+				});
+
+		}, 60000);				
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+//bots reaction to new messages
+/////////////////////////////////////////////////////////////////////////////////////////
+DiscordBot.on('message', msg => {
+
+////////////////////////////////////////////////////////////////////////////////////////
+//filters on channels
+/////////////////////////////////////////////////////////////////////////////////////////
+
+		switch(msg.channel.name) {
+
+			case 'trading':
+
+
+			break; 
+
+			case 'monitor':
+					setTimeout(function() {
+						msg.delete();
+
+					}, 60000 );
+
+			break;
+
+			 case 'bot':
+			 case 'general':
+			 case 'masternode-talk':
+
+				// ="" masternodes.online says every x hours 
+			 	if((msg.content.includes("rewards") || msg.content.includes("reward")) 
+			 		&& (msg.content.includes("no ") 	|| msg.content.includes("not ")	|| msg.content.includes("didn't")|| msg.content.includes("didnt")|| msg.content.includes("haven't") || msg.content.includes("havent")) 
+			 		&& (msg.content.includes("hours")|| msg.content.includes("hrs") || msg.content.includes("days") || msg.content.includes("day") || msg.content.includes("hours") || msg.content.includes("hour") ||msg.content.includes("time"))){
+			 	
+
+					var now = new Date();
+					var delay = 5 * 60000; // 60000 being the number of milliseconds in 15 minute
+					var Timeout=new Date(now-delay);
+
+					if (messageTriggered < Timeout) {
+					  messageTriggered = new Date();
+
+					}
+
+			 	}
+					
+			break; 
+		}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//filters on message
+// hint: messages can be easily created by using embed-visdualizer tool
+//https://leovoel.github.io/embed-visualizer/
+/////////////////////////////////////////////////////////////////////////////////////////
+ 		switch(msg.content) {
+   
+            case '!testwelcome':
+				  msg.author.send(JSON.parse(fs.readFileSync('welcomeMessage.json', 'utf8')));
+            	break;
+
+            case '!ping':
+           	 	msg.channel.send('pong');
+				break;
+
+			case "!help":
+				// msg.channel.send(JSON.parse(fs.readFileSync('help.json', 'utf8')));
+			break;
+
+			case "!upgrade":
+				// msg.channel.send(JSON.parse(fs.readFileSync('upgrade_old.json', 'utf8')));
+			break;
+			case "!swap":
+				// msg.channel.send(JSON.parse(fs.readFileSync('swap.json', 'utf8')));
+			break;
+
+			case "!boot":
+				// msg.channel.send(JSON.parse(fs.readFileSync('faq.json', 'utf8')));
+			break;
+
+
+			//don't use break if you want the same message for different commands
+			case "!help blockheight":
+			case "!help blockheigth":
+			case "!help block":
+				//calls 
+				exec(auth.cli+' getblockcount', function callback(error, stdout, stderr){
+										
+
+				    	setTimeout(function() {
+				    	
+				    		var temp_stak=fs.readFileSync('blockheight.json', 'utf8');
+
+				    		//sends blockheight.json messages but replaces <block_entry>
+							msg.channel.send(JSON.parse(temp_stak.replace(/<block_height>/g, stdout.match(/\d+/))));
+
+
+						}, 1000 );
+			
+						
+			});
+
+
+			break;
+
+			////////////////////////////////////////////////////////////////////////////////////////
+			//In case no static command could be found 
+			/////////////////////////////////////////////////////////////////////////////////////////
+			default:
+				var commandarray=msg.content.split(" ");
+				if(commandarray.length>2){
+					////////////////////////////////////////////////////////////////////////////////////////
+					//checks for !mn check <ip>:<port>
+					/////////////////////////////////////////////////////////////////////////////////////////
+					if((commandarray[0].includes("!mn")  && commandarray[1].includes("check"))){
+						if(typeof msg.channel.name !== "undefined"){
+
+							//checks if argument is an IP	
+							if(msg.channel.name.includes("bot")){
+  								result=commandarray[2].match(/^(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}:\d{1,6})/g);
+  									// console.log(result[0]);
+								
+								try {
+									if(result[0]){
+
+										    	datablock=0;
+										    	helpers.getDatablockofMNbyIP(result[0],function(datablock){
+
+
+											    	if(datablock!=0){
+											    		msg.channel.send("```IP:"+datablock.ip+"\nStatus:"   +datablock.status + "\nPublic-Key:"+ datablock.addr +  "\nVersion:"+datablock.version+"```\n Explorer:" +" https://xap2.ccore.online/address/"+datablock.addr);
+											    	}
+											    	else{
+											    		 msg.channel.send("```Couldn't find Masternode in Apollon Network.```");
+											    	}
+										    		
+										    	});
+
+										    	
+											
+									}else{
+										msg.channel.send("```Can't recognize " + result[0]+" as <ip>:<port>```");
+										return;
+
+									}
+
+								} catch (err) {
+								console.log("Error: on getting a correct IP  " );
+								}
+
+
+							}else{
+									//prints a message if you don't use the command in the bot channel (channel ID needed to produce link)
+										msg.channel.send("Please use the <#420317781054193676> channel");
+
+								}
+						}
+			
+						}
+					
+				}
+			
+				break;
+		}
+
+
+
+});
+
+
+//discord bot authentification
+DiscordBot.login(auth.discordtoken);
+
+
---
+++ user_modules/Helpers.js
@@ -0,0 +1,100 @@
+var fs = require('fs');
+const { exec } = require('child_process');
+const https = require('https');
+// require('ssl-root-cas').inject().addFile(__dirname + '/ssl/graviexnet.crt');
+module.exports = {
+newMembersfirstTrading: [],
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+//get time until specific date
+/////////////////////////////////////////////////////////////////////////////////////////
+getTimeuntil: function(time) {
+	
+    webinardate=new Date(time);
+    // webinardate=new Date('April 26, 2018 17:00:00');
+
+
+    today=new Date();
+
+    var seconds = Math.floor((webinardate - (today))/1000);
+    var minutes = Math.floor(seconds/60);
+    var hours = Math.floor(minutes/60);
+    var days = Math.floor(hours/24);
+
+    hours = hours-(days*24);
+    minutes = minutes-(days*24*60)-(hours*60);
+    seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
+    
+    return " "+days+" Days " +hours+" Hours "+minutes+" Minutes";
+},
+
+// ----receive function----v
+get_json: function(url, callback) {
+
+	 https.get(url, function(res) {
+      var body = '';
+ 
+      res.on('data', function(chunk) {
+         body += chunk;
+      });
+ 
+      res.on('end', function() {
+		try {
+           var result = JSON.parse(body)
+
+		} catch (err) {
+			console.log("parsing exception");
+    	  callback(JSON.parse('{"currentPriceBTC":0.0000,"currentPriceUSD":0.0,"currentPriceEUR":0.0,"btceur":0.0,"btcusd":0.0,"change24h":0.0,"coinsymbol":"XAP","exchangesBTC":[{"exchangeID":"cryptobridge","exchangeName":"Crypto bridge","exchangeDirectURL":"https://wallet.crypto-bridge.org/market/BRIDGE.XAP_BRIDGE.BTC","exchangeURL":"https://crypto-bridge.org/","updatetime":1526330503,"last":0.0,"buy":0.0,"sell":0.0000,"volume":0.0,"change":-2.43030736240171551107934239},{"exchangeID":"coinexchange","exchangeName":"Coinexchange","exchangeDirectURL":"https://www.coinexchange.io/market/XAP/BTC","exchangeURL":"https://www.coinexchange.io/","updatetime":1526331068,"last":0.0000,"buy":0.0000,"sell":0.000000,"volume":0.0,"change":9.14675767918088737201365188},{"exchangeID":"graviex","exchangeName":"Graviex","exchangeDirectURL":"https://graviex.net/markets/xapbtc","exchangeURL":"https://graviex.net/","updatetime":1526330503,"last":0.0,"buy":0.0,"sell":0.0,"volume":0.0,"change":-17.61313482022009062555169776},{"exchangeID":"palitanx","exchangeName":"Palitanx","exchangeDirectURL":"https://palitanx.com/exchange/BTC-XAP","exchangeURL":"https://palitanx.com/","updatetime":1526331219,"last":0.0,"buy":0.0,"sell":0.0,"volume":0.0,"change":0.0}]}'));
+
+		}
+
+        callback(result);
+      });
+
+
+    }).on('error', function(e) {
+      console.log("Error: " + e.message);
+    	callback(JSON.parse('{"currentPriceBTC":0.0000,"currentPriceUSD":0.0,"currentPriceEUR":0.0,"btceur":0.0,"btcusd":0.0,"change24h":0.0,"coinsymbol":"XAP","exchangesBTC":[{"exchangeID":"cryptobridge","exchangeName":"Crypto bridge","exchangeDirectURL":"https://wallet.crypto-bridge.org/market/BRIDGE.XAP_BRIDGE.BTC","exchangeURL":"https://crypto-bridge.org/","updatetime":1526330503,"last":0.0,"buy":0.0,"sell":0.0000,"volume":0.0,"change":-2.43030736240171551107934239},{"exchangeID":"coinexchange","exchangeName":"Coinexchange","exchangeDirectURL":"https://www.coinexchange.io/market/XAP/BTC","exchangeURL":"https://www.coinexchange.io/","updatetime":1526331068,"last":0.0000,"buy":0.0000,"sell":0.000000,"volume":0.0,"change":9.14675767918088737201365188},{"exchangeID":"graviex","exchangeName":"Graviex","exchangeDirectURL":"https://graviex.net/markets/xapbtc","exchangeURL":"https://graviex.net/","updatetime":1526330503,"last":0.0,"buy":0.0,"sell":0.0,"volume":0.0,"change":-17.61313482022009062555169776},{"exchangeID":"palitanx","exchangeName":"Palitanx","exchangeDirectURL":"https://palitanx.com/exchange/BTC-XAP","exchangeURL":"https://palitanx.com/","updatetime":1526331219,"last":0.0,"buy":0.0,"sell":0.0,"volume":0.0,"change":0.0}]}'));
+	});
+},
+
+
+
+getExtension: function(filename) {
+    var i = filename.lastIndexOf('.');
+    return (i < 0) ? '' : filename.substr(i);
+},
+
+
+getDatablockofMNbyIP: function(ip,callback){
+
+		exec(auth.cli+' masternode list > mnlist.txt',{
+    maxBuffer: 9000 * 1024 //quick fix
+    }, function waitofos(error, stdout, stderr){
+
+		// mnlist=stdout.toString().split(",");
+	   var obj=JSON.parse(fs.readFileSync("mnlist.txt", 'utf8')); // write it back 
+
+	     // console.log(obj.length)
+
+	    for (var i = obj.length - 1; i >= 0; i--) {
+
+	    	if(obj[i].ip.includes(ip)){
+	    		  // console.log("found MN in Apollon network");
+	    		 callback( obj[i]);
+	 			return;
+	    	}
+	    }
+
+	   callback(0)
+	    
+	 })
+
+}
+
+
+};
