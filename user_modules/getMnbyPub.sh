8926f1ce695ade9a9a30c948017c510d6756acae
ApollonWrock <mordeng10@gmail.com>
2019-03-20 14:59:38 -0400

    remove check for env variable

--- user_modules/getMnbyPub.sh
+++ user_modules/getMnbyPub.sh
@@ -1,4 +1,3 @@
 
 #!/bin/bash
-
-[ -z "$curluser" ] && [ -z "$apollonchainprovider" ] &&curl --user $curluser --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "listmasternodes", "params": [] }' -H 'content-type: text/plain;' $apollonchainprovider  | jq -c '.result' | jq -c '[ .[] | select(.addr | contains("AaVyUR7NX4XQPz3eAVrdqK9t4QYk9G2kNk")) ]'
+curl --user $curluser --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "listmasternodes", "params": [] }' -H 'content-type: text/plain;' $apollonchainprovider  | jq -c '.result' | jq -c '[ .[] | select(.addr | contains("AaVyUR7NX4XQPz3eAVrdqK9t4QYk9G2kNk")) ]'
