f66f67a6d45c63d2a815310f807aba42b7212585
ApollonWrock <mordeng10@gmail.com>
2019-03-20 14:36:13 -0400

    removed obsolete by ip fucntion

--- user_modules/Helpers.js
+++ user_modules/Helpers.js
@@ -92,36 +92,8 @@ getDatablockofMNbyPubkey: function(pubkey,callback){
 
      callback(0)
       
-   })
-
-},
-
-
-getDatablockofMNbyIP: function(ip,callback){
-
-		exec(auth.cli+' masternode list > mnlist.txt' ,{
-    maxBuffer: 9000 * 1024 //quick fix
-    }, function waitofos(error, stdout, stderr){
-
-		// mnlist=stdout.toString().split(",");
-	   var obj=JSON.parse(fs.readFileSync("mnlist.txt", 'utf8')); // write it back 
-// '
-	     // console.log(obj.length)
-
-	    for (var i = obj.length - 1; i >= 0; i--) {
-
-	    	if(obj[i].ip.includes(ip)){
-	    		  // console.log("found MN in Apollon network");
-	    		 callback( obj[i]);
-	 			return;
-	    	}
-	    }
-
-	   callback(0)
-	    
-	 })
+   });
 
 }
-
 
 };
